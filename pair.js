7632453a00fc41e0efef4beed4a1e1693f282f2f
rnKerm <149631529+RayanHack5@users.noreply.github.com>
2025-04-11 09:25:24 +0100

    Update pair.js

--- pair.js
+++ pair.js
@@ -3,7 +3,7 @@ import fs from 'fs';
 import path from 'path';
 import pino from 'pino';
 import { makeWASocket, useMultiFileAuthState, delay, makeCacheableSignalKeyStore, Browsers, jidNormalizedUser } from '@whiskeysockets/baileys';
-import { upload } from './mega.js'; // Assure-toi que le chemin est correct
+import { upload } from '../mega.js'; // Assure-toi que le chemin est correct
 
 const router = express.Router();
 
@@ -51,10 +51,9 @@ router.get('/', async (req, res) => {
                 await delay(10000);
                 const sessionPath = path.join(dirs, 'creds.json');
 
-                const megaUrl = await upload(fs.createReadStream(${dirs}/creds.json), ${generateRandomId()}.json);
-                    let stringSession = megaUrl.replace('https://mega.nz/file/', ''); // Extract session ID from URL
-                    stringSession = 'KERM-MD-V1~' + stringSession;  // Prepend your name to the session ID
-                
+                const megaUrl = await upload(fs.createReadStream(sessionPath), `session-${Date.now()}.json`);
+                let stringSession = 'KERM-MD-V1~' + megaUrl.replace('https://mega.nz/file/', '');
+
                 const userJid = jidNormalizedUser(num + '@s.whatsapp.net');
                 await GlobalTechInc.sendMessage(userJid, { text: stringSession });
 
